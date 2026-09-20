1f467078dbcaaf0a086d13622d3caf772260a1cc
Alisher-00kg <alisher.estebesov@gmail.com>
2026-09-21 02:41:47 +0600

    add rolesGuard

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,12 +1,15 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
   create(@Body() body: CreateProductDto) {
     return this.productsService.create(
       body.name,
--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ProductsService } from './products.service';
+import { PassportModule } from '@nestjs/passport';
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+  ],
+  controllers: [ProductsController],
   providers: [ProductsService],
-  controllers: [ProductsController]
 })
 export class ProductsModule {}
